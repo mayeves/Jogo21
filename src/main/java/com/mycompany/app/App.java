5a952c207b743692cff49bde307c5b22a419870c
mayeves <114372236+mayeves@users.noreply.github.com>
2023-09-26 21:22:09 +0000

    Tratamento baralho e entradas via console

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,16 +5,66 @@
 
 package com.mycompany.app;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        
+    private static Scanner in = new Scanner(System.in);
+    static Baralho baralho = new Baralho();
+    static int totalPontos = 0;
+    public static void main(String[] args) throws Exception{
+        int jogadores = 1;
 
-        Baralho baralho = new Baralho();
+        System.out.println("Informe a quantidade de jogadores");
+        jogadores = in.nextInt();
+
         baralho.criarBaralho();
-        String nome = baralho.sortearCarta();
-
-        System.out.println("A carta sorteada é a: "+nome);
         
+        primeiraMao();
+        jogadasAdicionais();
+
+        
+        
+    }
+
+    static void primeiraMao(){
+        for (int i=0; i<1; i++){
+            Carta cartaSorteada = new Carta();
+            cartaSorteada = baralho.sortearCarta();
+            totalPontos = totalPontos + cartaSorteada.valor;
+            System.out.println("A carta sorteada é a: "+cartaSorteada.nome+" de "+cartaSorteada.getNaipe());
+            System.out.println("Seu total é de "+totalPontos+" pontos até o momento!");
+
+        }
+    }
+
+     /**
+     * @throws Exception
+     */
+    static void jogadasAdicionais() throws Exception{
+        String continuar = "N";
+        int totalCartas;
+
+        System.out.println("Informe se deseja solicitar outra carta: ");
+        continuar = in.nextLine();
+
+        do {
+            totalCartas = Baralho.baralho.size();
+            Carta cartaSorteada = new Carta();
+            cartaSorteada = baralho.sortearCarta();
+            totalPontos = totalPontos + cartaSorteada.valor;
+            System.out.println("A carta sorteada é a: "+cartaSorteada.nome+" de "+cartaSorteada.getNaipe());
+            System.out.println("Seu total é de "+totalPontos+" pontos até o momento!");
+            System.out.println("Informe se deseja solicitar outra carta: ");
+            continuar = in.nextLine();
+
+        } while (continuar.equals("S") || continuar.equals("s"));
+
+        System.out.println("Final do jogo com o total de "+totalPontos);
+
+
+        
+
+
         
     }
 }
--- src/main/java/com/mycompany/app/Baralho.java
+++ src/main/java/com/mycompany/app/Baralho.java
@@ -6,65 +6,52 @@ import java.util.Random;
 
 public class Baralho {
 
-    List<Carta> baralho = new ArrayList<Carta>();
+    static List<Carta> baralho = new ArrayList<Carta>();
 
-    public String sortearCarta(){
+    public Carta sortearCarta(){
         Random random = new Random();
-        int indice = random.nextInt(30);
-
         Carta carta = new Carta();
-        carta = baralho[indice];
-        return carta.getNome();
+        if(baralho.size() < 1){
+            System.out.println("O baralho terminou");
+            return null;
+        }
+        int indice = random.nextInt(baralho.size());
+        carta = baralho.get(indice);
+        baralho.remove(indice);
+        return carta;
+
     }
     public void criarBaralho(){
-        baralho = new ArrayList<Carta>();
-
-        String[] naipe = {"Copas","Espada", "Paus", "Ouro"};
+         String[] naipe = {"Copas","Espada", "Paus", "Ouro"};
        
-        
         for (int naipes=0; naipes<4;naipes++){            
               Carta carta = new Carta();
               carta.setCarta("AS", naipe[naipes], 1);
               baralho.add(carta);
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size());              
               carta.setCarta("DOIS", naipe[naipes], 2);
               baralho.add(carta);
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size());              
               carta.setCarta("TRES", naipe[naipes], 3);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("QUATRO", naipe[naipes], 4);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("CINCO", naipe[naipes], 5);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("SEIS", naipe[naipes], 6);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("SETE", naipe[naipes], 7);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("OITO", naipe[naipes], 8);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("NOVE", naipe[naipes], 9);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("DEZ", naipe[naipes], 10);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("VALETE", naipe[naipes], 11);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("DAMA", naipe[naipes], 11);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
               carta.setCarta("REI", naipe[naipes], 11);
               baralho.add(carta); 
-              System.out.println("Carta: "+carta.getNome()+carta.getNaipe()+baralho.size()); 
-                         
-                        
         }
     }         
     
